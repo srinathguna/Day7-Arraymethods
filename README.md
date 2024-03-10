56f196a622f9c538fac7b41eec25475b284ca9d1
SrinathG_Contus <srinath.g@contus.in>
2024-03-10 23:09:54 +0530

    updated ouput and readme file

---
+++ README.md
@@ -0,0 +1,47 @@
+# Day 7 Task
+
+## Table of Contents
+- Description
+- About project
+- How to access
+- Output screenshots
+## Description
+
+**Project task about below points**
+
+**Done the below list**
+Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
+1. Get all the countries from Asia continent /region using Filter function
+2. Get all the countries with a population of less than 2 lakhs using Filter function
+3. Print the following details name, capital, flag, using forEach function
+4. Print the total population of countries using reduce function
+5. Print the country that uses US dollars as currency.
+
+
+## About project
+
+1. Clone this repository to your local machine.
+   ```bash
+   git clone [https://github.com/srinathguna/Day7-Arraymethods.git]
+   ```
+
+2. Navigate to the project directory.
+   ```bash
+   cd Day7-Arraymethods
+   ```
+
+## How to access
+This project contains the tasks of 
+open the folder,
+Run the file separate to view the output
+1. open the folder view the js file.
+2. Run the index file - list of programs details
+3. Run  js file > choose inspect > choose console tab - output of respected questions
+
+## Output Screen shots
+
+
+
+
+
+
--- arraymethods.js
+++ arraymethods.js
@@ -4,27 +4,32 @@ xhr.open("GET", URL);
 xhr.send();
 xhr.onload = () => {
     let data = JSON.parse(xhr.response)
-
     //Get all the countries from Asia continent /region using Filter function
+    console.log("--- Output asia countries ---")
     let countries = data.filter((val) => val.continents.includes("Asia") && val.region === "Asia") //Filter
-    console.log(countries); //Output asia cpuntries
+    console.log(countries); //Output asia countries
 
     //Get all the countries with a population of less than 2 lakhs using Filter function
+    console.log("--- Output countries with population ---")
     let population = data.filter((val) => val.population < 200000) //Filter
     console.log(population); //Output countries with population
 
     //Print the following details name, capital, flag, using forEach function
+    console.log("--- Output all countries name,captital,flag ---")
     data.forEach(element => (
         console.log(`Name:${element.name.common } \nCapital:${element.capital} \nFlag: ${element.flags.png} `)
-    ));
+    )); //Output all countries name,captital,flag
+
 
     //Print the total population of countries using reduce function
+    console.log("--- output population ---")
     let totalPopulation = data.reduce((pval,cval) => {
         return pval + cval.population
     },0)
-    console.log(totalPopulation.toLocaleString()) // output population 
+    console.log(totalPopulation.toLocaleString()) // output population
 
     //Print the country that uses US dollars as currency.
+    console.log("--- Output uscurrency ---")
     let usCurrency = data.filter((val) => val.currencies?.hasOwnProperty('USD')) //Filter
     console.log(usCurrency); //Output uscurrency
 }
